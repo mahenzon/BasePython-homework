18c92ce0716209e8601bc2e2411fe063621f00a6
snvtr@mail.ru <snvtr@mail.ru>
2021-03-15 23:32:37 +0300

    main.py

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,14 +1,22 @@
+#!/usr/bin/python3
+
 """
 Домашнее задание №1
 Функции и структуры данных
 """
 
-def power_numbers(NumList):
+def power_numbers(*num_list):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     """
-    return [i*i if isinstance(i, int) else i for i in NumList]
+    Result = list()
+    for i in num_list:
+        if isinstance(i, int):
+            Result.append(i*i)
+        else:
+            Result.append(i)
+    return Result
 
 # filter types
 ODD = "odd"
@@ -28,9 +36,20 @@ def filter_numbers(NumList, condition):
     """
 
     if condition == ODD:
-        result = list(filter(lambda i: i % 2 != 0, NumList))
-    
-    if condition == EVEN:
-        result = list(filter(lambda i: i % 2 == 0, NumList))
+        Result = list(filter(lambda i: i % 2 != 0, NumList))
 
-    return result
+    if condition == EVEN:
+        Result = list(filter(lambda i: i % 2 == 0, NumList))
+
+    if condition == PRIME:
+        Result = list()
+        for i in NumList:
+            is_prime = True
+            for j in range(2, i):
+                if i % j == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                Result.append(i)
+
+    return Result
