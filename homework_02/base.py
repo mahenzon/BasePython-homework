5a0b75d30901833b80da3fd839a0e3f4f886c248
Alexander Ananchenko <snvtr@mail.ru>
2021-04-06 09:17:28 +0300

    Update base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from exceptions import LowFuelError, NotEnoughFuel, CargoOverload
+from homework_02.exceptions import LowFuelError, NotEnoughFuel, CargoOverload
 
 
 class Vehicle(ABC):
