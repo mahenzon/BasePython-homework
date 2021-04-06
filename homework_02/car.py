c2bb682a6206b6eee2a0631a1cef454b9afcd61b
Alexander Ananchenko <snvtr@mail.ru>
2021-04-06 09:18:27 +0300

    Update car.py

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,8 +1,8 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
-from homework02.base import Vehicle
-from homework02.engine import Engine
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
 
 class Car(Vehicle):
     """ defaults """
